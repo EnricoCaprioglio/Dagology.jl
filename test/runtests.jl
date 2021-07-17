db7c61f226a6cc637ba1f54e641a2724dfa56bb1
Burromarmellata <enricocaprioglio@yahoo.com>
2021-07-17 19:54:30 +0100

    Update runtests.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,3 +1,25 @@
+using Dagology
 using Test
+using LightGraphs
+
+# test degree_distr
+g = SimpleDiGraph(); # this is a digraph
+typeof(g)
+A = rand(1000, 2)*1000;
+# for i in 1:size(A)[1]
+for i in 1:4
+    add_edge!(g, round(Int, A[i,1]), round(Int, A[i,2]))
+end
+
+g.fadjlist # out degree adjacency list
+g.badjlist # in degree adjacency list
+g.ne       # number of vertices
+# test function
+k_out, k_in = degree_distr(g);
+#############################################################
+
 
 @test true
+@testset "Dagology" begin
+
+end
