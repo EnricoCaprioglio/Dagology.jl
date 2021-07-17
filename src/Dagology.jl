c5cfb75ec86faaccc0aef37b1d50e51ae92a9bc4
Burromarmellata <enricocaprioglio@yahoo.com>
2021-07-17 19:54:40 +0100

    Update Dagology.jl

--- src/Dagology.jl
+++ src/Dagology.jl
@@ -1,5 +1,13 @@
 module Dagology
 
-greet() = print("Hello World!")
+# greet() = print("Hello World!")
+
+# files you want to include
+include("degree_distr.jl")
+
+
+# functions to export
+# these functions will be ready to use when using Dagology
+export degree_distr
 
 end # module
