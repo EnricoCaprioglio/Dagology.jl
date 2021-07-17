7d73d864d8f2567c25ff54ebd8bcad4767702946
Burromarmellata <enricocaprioglio@yahoo.com>
2021-07-18 00:33:12 +0100

    Update Dagology.jl

--- src/Dagology.jl
+++ src/Dagology.jl
@@ -4,7 +4,8 @@ module Dagology
 
 # files you want to include
 include("degree_distr.jl")
-
+include("Box_Space.jl")
+include("Plotting_DAGs.jl")
 
 # functions to export
 # these functions will be ready to use when using Dagology
