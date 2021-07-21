e91e41fd1e3da444fd415513405904a12df694be
Burromarmellata <enricocaprioglio@yahoo.com>
2021-07-21 21:54:33 +0100

    Update longest_path.jl

--- src/longest_path.jl
+++ src/longest_path.jl
@@ -1,10 +1,13 @@
-# We are going to use topological sort by dfs from
-# LightGraphs package
-# topological_sort_by_dfs(g)
-
+using Base: Int32, Int64
 using LightGraphs
 
-function find_sources(g)
+function find_sources(g::SimpleDiGraph{Int64})
+    """
+    find_source(g::SimpleDiGraph{Int64})
+
+    returns an array containing the nodes with
+    in-degree equal to Zero, i.e. k^{in} = 0
+    """
     return findall(x->(x==0), [length(i) for i ∈ g.badjlist])
 end
 
@@ -21,61 +24,71 @@ function _dfs_for_TopSort(index, start, visited, order, g)
     return index-1, visited, order
 end
 
+# My version of topological sort function
+# notice the packaeg LighGraphs has the similar function
+# topological_sort_by_dfs, I need to check what are the differences
 function my_TopSort_by_dfs(g::SimpleDiGraph{Int64})
-    index = size(g)[1];
+    index = Int64(size(g)[1]);
     visited = falses(size(g)[1]);
     order = zeros(size(g)[1]);
     starts = find_sources(g);
     for start in starts
         index, visited, order = _dfs_for_TopSort(index, start, visited, order, g);
     end
-    return order
+    return Array{Int64}(order)
 end
 
-# Single Source Shortest Path
-# this function outputs all the shortest distances
+# Single Source Longest Path
+# this function outputs all the longest distances
 # from starting node to any other node > starting node
-function _dfs_for_sssp(g, start, dist, count)
-    neigh = g.fadjlist[start]; # get neighbours of starting node
-    new_dist = zeros(length(dist));
-    for v in neigh
-        new_dist[v] = 1 +  new_dist[start]
-        if visited[v] == false
-            count += 1;
-            count = _dfs_for_sssp(g, start, dist, count)
+function my_sslp(g::SimpleDiGraph{Int64}, order::Array{Int64, 1}, start::Int64)
+    N = Int64(size(g)[1]);
+    get_pos = findall(x->(x==start), order);
+    dist = zeros(N).-Inf;
+    dist[start] = 0;
+    # loop over all elements > start in the top_sort poset
+    # exclude elements that cann't be reached by node start
+    for (top_sort_i, elem) ∈ enumerate(order[get_pos[1]:end])
+        neigh_arr = g.fadjlist[elem];
+        counter = Int64(1);
+        neigh_index = Int64(1);
+        while counter <= length(neigh_arr)
+            neigh = order[top_sort_i+neigh_index];
+            if neigh ∈ neigh_arr
+                new_dist = dist[elem] + 1;
+                dist[neigh] = maximum([new_dist, dist[neigh]])
+                counter += 1; # neighbour found, move to next
+            end
+            neigh_index += 1; # to find next element of topsort
         end
     end
-    return count
+    return dist
 end
 
-function sssp(g, start)
-    N = size(g)[1];
-    order = topological_sort_by_dfs(g);
-    get_pos = findall(x->(x==start), order);
-    max_dist = (N-get_pos)
-    dist = zeros(N).+max_dist;
-    for i ∈ order[get_pos:end]
-        # dist = _dfs_for_sssp(g, dist)
+function _relax_neighs(dist::Array{Float64}, elem, g)
+    neigh_arr = g.fadjlist[elem];
+    for neigh in neigh_arr
+        new_dist = dist[elem] + 1;
+        dist[neigh] = maximum([new_dist, dist[neigh]])
     end
+    return dist
 end
 
-function sssp(g, start)
-    N = size(g)[1];
-    order = topological_sort_by_dfs(g);
+function my_sslp_v2(g::SimpleDiGraph{Int64}, order::Array{Int64, 1}, start::Int64)
+    N = Int64(size(g)[1]);
     get_pos = findall(x->(x==start), order);
-    max_dist = (N-get_pos)
-    dist = zeros(N).+max_dist;
+    dist = zeros(N).-Inf;
     dist[start] = 0;
-    for i ∈ order[get_pos:end]
-        # dist = _dfs_for_sssp(g, dist)
+    _relax_neighs(dist, start, g)
+    for (top_sort_i, elem) ∈ enumerate(order[get_pos[1]:end])
+        if dist[elem] != -Inf;
+            dist = _relax_neighs(dist, elem, g)
+        else
+            continue
+        end
     end
+    return dist
 end
-
-# the longest path is simply the exact same function
-# for the shortest path but instead of adding 1 at each
-# step to calculated the distance we add -1, then
-# the shortest distance will actually be the longest.
-
 
 ##########################################################################
 # example test
@@ -102,6 +115,7 @@ add_edge!(g, 9, 12);
 add_edge!(g, 10, 13);
 
 ##########################################################################
+# plot test graph
 using GraphPlot
 nodelabel = ["A","B","C","D","E","F","G","H","I","J","K","L","M"];
 nodesize = [LightGraphs.outdegree(g, v) for v in LightGraphs.vertices(g)];
@@ -110,34 +124,30 @@ g.fadjlist    # out degree adjacency list
 g.badjlist    # in degree adjacency list
 g.ne          # number of edges
 
-
-##########################################################################
-# sssp tests
-N = size(g)[1];
-order = topological_sort_by_dfs(g);
-start = 8;
-get_pos = findall(x->(x==start), order);
-max_dist = (N-get_pos[1])
-dist = zeros(N).+(max_dist+1);
-dist[start] = 0;
-for i ∈ order[get_pos[1]:end]
-    neigh = g.fadjlist[start];
-    
-    # dist = _dfs_for_sssp(g, dist)
-end
-
 ##########################################################################
 # test functions defined above
-my_TopSort_by_dfs(g)
+order = my_TopSort_by_dfs(g);
+typeof(order)
+order = topological_sort_by_dfs(g);
+typeof(order)
+starts = find_sources(g);
+start = round(Int, starts[1]);
 
-find_sources(g);
+dist = my_sslp(g, order, start)
+dist = my_sslp_v2(g, order, start)
 
+##########################################################################
+# Benchmark functions
 using BenchmarkTools
 @btime topological_sort_by_dfs(g)
 @btime my_TopSort_by_dfs(g)
 # my Topological Sort function seems to be quicker but that seems quite
 # weird, there might be some mistakes or some cases I have not considered
 # keep this in mind.
+
+@btime dist = my_sslp(g, order, start)
+@btime dist = my_sslp_faster(g, order, start)
+# quite interestingly my_sslp_faster is actually slower than my_sslp
 ##########################################################################
 
 using Test
