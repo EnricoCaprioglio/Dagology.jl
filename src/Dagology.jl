2a1da205b17c5554701c1f88f49fab41b7a4b9c0
Burromarmellata <enricocaprioglio@yahoo.com>
2021-07-21 21:54:29 +0100

    Update Dagology.jl

--- src/Dagology.jl
+++ src/Dagology.jl
@@ -6,9 +6,14 @@ module Dagology
 include("degree_distr.jl")
 include("Box_Space.jl")
 include("Plotting_DAGs.jl")
+include("longest_path.jl")
 
 # functions to export
 # these functions will be ready to use when using Dagology
 export degree_distr
+export find_sources
+export my_TopSort_by_dfs
+export my_sslp
+export my_sslp_v2
 
 end # module
