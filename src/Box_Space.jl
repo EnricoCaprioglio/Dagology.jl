1ab201829c329b137f69ea0be4b03a55bd5f426e
Burromarmellata <enricocaprioglio@yahoo.com>
2021-07-24 15:42:07 +0100

    Update Box_Space.jl

--- src/Box_Space.jl
+++ src/Box_Space.jl
@@ -60,18 +60,18 @@ end
                      node i.
         g            Directed Acyclic Graph, g::SimpleDiGraph{Int64}
 """
-# function cone_space_digraph(N::Int64, d::Int64, p::Float16 = 1.0, R::Float16 = Inf16)
-#     time_vec = rand(N);                     # times
-#     sort!(time_vec);                        # time ordering
-#     Box_pos = hcat(time_vec,rand(N,d-1));   # positions
-#     g = SimpleDiGraph(N);
-#     for i in 1:N
-#         for j in i:N
-#             time_sep = abs(norm(Box_pos[j,2:d]) - norm(Box_pos[i,2:d]))
-#             if time_sep < R && isless(rand(1)[1],p)
-#                 add_edge!(g, i, j);
-#             end
-#         end
-#     end
-#     return Box_pos, g
-# end
+function cone_space_digraph(N::Int64, d::Int64, p::Float16 = 1.0, R::Float16 = Inf16)
+    time_vec = rand(N);                     # times
+    sort!(time_vec);                        # time ordering
+    Box_pos = hcat(time_vec,rand(N,d-1));   # positions
+    g = SimpleDiGraph(N);
+    for i in 1:N
+        for j in i:N
+            time_sep = abs(norm(Box_pos[j,2:d]) - norm(Box_pos[i,2:d]))
+            if time_sep < R && isless(rand(1)[1],p)
+                add_edge!(g, i, j);
+            end
+        end
+    end
+    return Box_pos, g
+end
