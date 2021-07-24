7a78f5b5b09e64cf33881e4984d851bead076c42
Burromarmellata <enricocaprioglio@yahoo.com>
2021-07-24 02:41:24 +0100

    Update longest_path.jl

--- src/longest_path.jl
+++ src/longest_path.jl
@@ -1,13 +1,13 @@
 using Base: Int32, Int64
 using LightGraphs
 
-function find_sources(g::SimpleDiGraph{Int64})
-    """
+"""
     find_source(g::SimpleDiGraph{Int64})
 
     returns an array containing the nodes with
     in-degree equal to Zero, i.e. k^{in} = 0
-    """
+"""
+function find_sources(g::SimpleDiGraph{Int64})
     return findall(x->(x==0), [length(i) for i ∈ g.badjlist])
 end
 
@@ -25,8 +25,19 @@ function _dfs_for_TopSort(index, start, visited, order, g)
 end
 
 # My version of topological sort function
-# notice the packaeg LighGraphs has the similar function
+# notice the package LighGraphs has the similar function
 # topological_sort_by_dfs, I need to check what are the differences
+"""
+Simple implementation of DFS algorithm to find the topological order of a DAG.
+Recall, the topological order of a DAG is not unique.
+
+Only one input:
+    g::SimpleDiGraph{Int64}     The DAG
+
+output:
+    order::Array{Int64}         A list with elements in the topological order
+
+"""
 function my_TopSort_by_dfs(g::SimpleDiGraph{Int64})
     index = Int64(size(g)[1]);
     visited = falses(size(g)[1]);
@@ -41,25 +52,30 @@ end
 # Single Source Longest Path
 # this function outputs all the longest distances
 # from starting node to any other node > starting node
-function my_sslp(g::SimpleDiGraph{Int64}, order::Array{Int64, 1}, start::Int64)
+
+function my_sslp(g::SimpleDiGraph{Int64}, order::Array{Int64, 1},
+    start::Int64, print = false)
     N = Int64(size(g)[1]);
-    get_pos = findall(x->(x==start), order);
-    dist = zeros(N).-Inf;
+    get_pos = findall(x->(x==start), order);        # get starting position
+    dist = zeros(N).-Inf;                           # initialize distance array
     dist[start] = 0;
     # loop over all elements > start in the top_sort poset
-    # exclude elements that cann't be reached by node start
+    # exclude elements that can't be reached by node start
     for (top_sort_i, elem) ∈ enumerate(order[get_pos[1]:end])
-        neigh_arr = g.fadjlist[elem];
-        counter = Int64(1);
-        neigh_index = Int64(1);
-        while counter <= length(neigh_arr)
-            neigh = order[top_sort_i+neigh_index];
-            if neigh ∈ neigh_arr
-                new_dist = dist[elem] + 1;
-                dist[neigh] = maximum([new_dist, dist[neigh]])
-                counter += 1; # neighbour found, move to next
+    if print
+        println("\n We are at element: $elem, in place $top_sort_i in the TopSort order,
+         element $elem is at distance: $(dist[elem]) from the source $start.")
+    end
+        if dist[elem] == -Inf       # no path between source and elem
+            continue
+        else                        # update neighbours distances
+            neigh_arr = g.fadjlist[elem];
+            for neigh in neigh_arr
+                dist[neigh] = maximum([(dist[elem] + 1), dist[neigh]]);
+                if print
+                    println("The distance of $neigh, neighbour of $elem, is $(dist[neigh])")
+                end
             end
-            neigh_index += 1; # to find next element of topsort
         end
     end
     return dist
@@ -74,6 +90,9 @@ function _relax_neighs(dist::Array{Float64}, elem, g)
     return dist
 end
 
+# Just a second version of the single source longest path algorithm.
+# Not sure if calling anoter function to relax the disatnces is faster or
+# slower.
 function my_sslp_v2(g::SimpleDiGraph{Int64}, order::Array{Int64, 1}, start::Int64)
     N = Int64(size(g)[1]);
     get_pos = findall(x->(x==start), order);
@@ -89,5 +108,3 @@ function my_sslp_v2(g::SimpleDiGraph{Int64}, order::Array{Int64, 1}, start::Int6
     end
     return dist
 end
-
-
