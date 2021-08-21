b4f2f284a2b706a289237a0dc07b4f272ff9de50
Burromarmellata <enricocaprioglio@yahoo.com>
2021-08-21 21:04:44 +0100

    Update Minkowski_dist.jl

--- src/Minkowski_dist.jl
+++ src/Minkowski_dist.jl
@@ -8,7 +8,7 @@ This is the distance that we can use in cube space.
 function d_minkowski(x, y, d, p)
     result = 0;
     for i in 1:d
-        result += (x[i]-y[i])^p;
+        result += abs.(x[i]-y[i])^p;
     end
     return result^(1/p)
 end
