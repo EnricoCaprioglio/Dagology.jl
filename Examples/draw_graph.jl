9910e114aa39ddd353756b6a9a57709f1a6e3576
Burromarmellata <enricocaprioglio@yahoo.com>
2021-08-25 19:20:06 +0100

    Update draw_graph.jl

--- Examples/draw_graph.jl
+++ Examples/draw_graph.jl
@@ -7,12 +7,12 @@ using Plots
 ##########################################################################
 # Plot a 2D graph
 # set up data (this representation works only for d=2)
-p = 0.5; N = 500; d = 2; fraction = 10;
+p = -0.5; N = 100; d = 2; perc = 10;
 max_R = d_minkowski(ones(N), zeros(N), d, p);
 
 # choose the kind of graph to use
 # (pos, g) = cube_space_digraph(N,d);
-(pos, g) = cube_space_digraph(N, d, max_R/fraction, p);
+(pos, g) = cube_space_digraph(N, d, (max_R/100)*perc, p);
 # (pos, g) = static_cube_space(N, d, max_R/fraction, p)
 
 # find longest path distances
@@ -26,7 +26,7 @@ dst = longest_path_vertices[1];
 ds = dijkstra_shortest_paths(g,1,weights(g));
 shortest_path_vertices = get_shortest_path_vertices(adjlist, ds.dists, dst, pos, p);
 my_plot = DAG_plot_2D(g, pos, longest_path_vertices, 
-shortest_path_vertices, false, false, false, false, true)
+shortest_path_vertices, false, false, true, false, true)
 
 ##########################################################################
 # to save the plot
@@ -34,3 +34,4 @@ using Compose, Cairo
 draw(PDF("p_more_1_example.pdf", 16cm, 16cm), DAG_plot_2D(g, pos, longest_path_vertices, shortest_path_vertices, false, false, true, false, true))
 # draw(PNG("test_graph.png", 16cm, 16cm), DAG_plot_2D(g, pos, longest_path_vertices, shortest_path_vertices, false, false, true, false))
 # draw(SVG("test_graph.svg", 16cm, 16cm), DAG_plot_2D(g, pos, longest_path_vertices, shortest_path_vertices, false, false, true, false))
+
