7975af0f5487fc0efe335445c8cb0deaa0f05410
Burromarmellata <enricocaprioglio@yahoo.com>
2021-08-28 21:00:29 +0100

    Update Benchmarking.jl

--- test/Benchmarking.jl
+++ test/Benchmarking.jl
@@ -6,15 +6,15 @@ Random.seed!(54)
 
 ##########################################################################
 # for all tests you need to run the following
-N = 100; d = 2; p0 = 1.5; fraction = 1;
+N = 1000; d = 2; p = 1.5; perc = 15; prob = 1.0;
 # max_R = d_minkowski(ones(N), zeros(N), d, p0);
 max_R = Inf64
-(pos, g) = cube_space_digraph(N, d, max_R/fraction)
+(pos, g) = cube_space_digraph(N, d, max_R*perc/100)
 
 ##########################################################################
-@btime (pos, g) = cube_space_digraph(N, d)
-@btime (pos, g) = static_cube_space(N, d, max_R/fraction)
-@btime (pos, g) = cone_space_digraph_test(N, d, 1, Inf64)
+@btime (pos, g) = cube_space_digraph(N, d, max_R*perc/100, p, prob)
+@btime (pos, g) = static_cube_space(N, d, max_R*perc/100, p, prob)
+@btime (pos, g) = cone_space_digraph(N, d, max_R*perc/100, prob)
 
 ##########################################################################
 @btime topological_sort_by_dfs(g)
@@ -24,36 +24,42 @@ max_R = Inf64
 # keep this in mind.
 
 ##########################################################################
-(pos, g) = static_cube_space(N, d, max_R/fraction)
+(pos, g) = cone_space_digraph(N, d, max_R*perc/100, prob)
 order = topological_sort_by_dfs(g);     # using lightGraphs function
 
 @btime dist = my_sslp(g, order, 1)
 @btime dist = my_sslp_v2(g, order, 1)
-# quite interestingly my_sslp_faster is actually slower than my_sslp
+
+# Redefine weigths to use them for shortest path algorithms
 new_weights = weights(g).*(-1)
 ds = dijkstra_shortest_paths(g,1,new_weights)
 ds.dists
 ds2 = dijkstra_shortest_paths(g,1)
 ds2.dists
+# Check "dijkstra longest paths comparison" in longest_path_test.jl for comparison
+# between my single source longest path algorithm output and dijkstra algorithm
+# used to find the longest path.
 
 ##########################################################################
 # Benchmark dijkstra_shortest_paths and compare with my_sslp
-N = 10000; d = 2;
-p0 = 1.5; fraction = 5;
-max_R = d_minkowski(ones(N), zeros(N), d, p0);
-(pos, g) = cube_space_digraph(N, d, max_R/fraction, p0);
-order = topological_sort_by_dfs(g);     # using lightGraphs function
-@btime my_sslp(g, order, 1)
+p = 1.5; N = 1000; d = 2; perc = 15;
+max_R = Inf64
+(pos, g) = cube_space_digraph(N, d, max_R*perc/100, p);
+order = topological_sort_by_dfs(g);
 dist = my_sslp(g, order, 1)
 new_weights = weights(g).*(-1);
-@btime dijkstra_shortest_paths(g,1,new_weights)
 ds = dijkstra_shortest_paths(g,1,new_weights)
-dist == ds.dists.*(-1)
-# so my_sslp uses more memory but it's faster
+@test dist == ds.dists.*(-1)
+@btime my_sslp(g, order, 1)
+@btime dijkstra_shortest_paths(g,1,new_weights)
+# so my_sslp uses MORE memory but it's faster.
+
+# TODO: understand why when max_R is different the two
+# algorithms find some paths differently.
 
 ##########################################################################
 # compare with static DiGraph
-(pos, g) = static_cube_space(N, d, max_R/fraction, p0);
+(pos, g) = static_cube_space(N, d, max_R*perc/100, p);
 order = topological_sort_by_dfs(g);     # using lightGraphs function
 new_weights = weights(g).*(-1);
 @btime ds = dijkstra_shortest_paths(g,1,new_weights)
