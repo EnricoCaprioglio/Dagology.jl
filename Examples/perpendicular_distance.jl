f38684338b1443ffb45fcd7764436733f2c48fb6
Burromarmellata <enricocaprioglio@yahoo.com>
2021-08-28 21:00:13 +0100

    Update perpendicular_distance.jl

--- Examples/perpendicular_distance.jl
+++ Examples/perpendicular_distance.jl
@@ -3,33 +3,29 @@ using LightGraphs
 using Distributions
 
 ##########################################################################
-# set up data
-p = 0.25; N = 1000; d = 2; fraction = 10;
+###############
+# Set up data #
+###############
+p = 0.25; N = 1000; d = 2; perc = 10;
 max_R = d_minkowski(ones(d), zeros(d), d, p);
-##########################################################################
-# choose the kind of graph to use
+## Choose the kind of graph to use
 # (pos, g) = cube_space_digraph(N,d);
-(pos, g) = cube_space_digraph(N, d, max_R/fraction, p);
-# (pos, g) = static_cube_space(N, d, max_R/fraction, p)
+(pos, g) = cube_space_digraph(N, d, max_R*perc/100, p);
 adjlist = g.badjlist;
-##########################################################################
-# find longest path
+## Find longest path
 dists = my_sslp(g, topological_sort_by_dfs(g), 1);
 value_longest_path = maximum(dists);
 vertex_longest_path = findall(x -> x == value_longest_path, dists)[1];
 longest_path_vertices = get_longest_path_vertices(adjlist, dists, pos, p);
 # println("This is the longest path: ", longest_path_vertices[end:-1:1])
-##########################################################################
-# find shertest path
+## Find shortest path
 dst = longest_path_vertices[1];
 ds = dijkstra_shortest_paths(g,1,weights(g));
 shortest_path_vertices = get_shortest_path_vertices(adjlist, ds.dists, dst, pos, p);
-##########################################################################
-# Uncomment if you want to plot
+## Uncomment if you want to plot
 my_plot = DAG_plot_2D(g, pos, longest_path_vertices, 
 shortest_path_vertices, false, false, true, false, true)
-##########################################################################
-# calculate average perpendicular distance
+## Calculate average perpendicular distance
 long_sum = 0;
 for i in 1:(length(longest_path_vertices)-1)
     x = pos[longest_path_vertices[i],:]
@@ -39,7 +35,7 @@ for i in 1:(length(longest_path_vertices)-1)
         t += j
     end
     y = ones(d).*(t/d)
-    # p = 2 # if you want euclidean distance
+    # p = 2 # if you want Euclidean distance
     long_sum += d_minkowski(x,y,d,p)
 end
 short_sum = 0;
