77a3808ea8f8381ff414778431ca081ad72c226a
Burromarmellata <enricocaprioglio@yahoo.com>
2021-09-08 22:44:01 +0100

    Update Plotting_DAGs.jl

--- src/Plotting_DAGs.jl
+++ src/Plotting_DAGs.jl
@@ -13,7 +13,7 @@ using GraphPlot
 function DAG_plot_2D(g, pos, l_path, s_path,
     nodesizes = false, nodefillcs = false,
     rotated = false, nodelabels = false,
-    edgestrokes = false)
+    edgestrokes = false, arrowlen = 0.01)
 
     if length(pos[1,:]) != 2
         throw(DomainError(:wrongdimension))
@@ -100,24 +100,24 @@ function DAG_plot_2D(g, pos, l_path, s_path,
             nodelabel = collect(1:N)
             my_plot = gplot(g, x_prime, y_prime,
             nodefillc=nodefillc, nodesize=nodesize,
-            nodelabel=nodelabel, arrowlengthfrac = 0.01,
+            nodelabel=nodelabel, arrowlengthfrac = arrowlen,
             edgestrokec=edgestrokec)
         else
             my_plot = gplot(g, x_prime, y_prime,
             nodefillc=nodefillc, nodesize=nodesize,
-            arrowlengthfrac = 0.01, edgestrokec=edgestrokec)
+            arrowlengthfrac = arrowlen, edgestrokec=edgestrokec)
         end
     else
         if nodelabels
             nodelabel = collect(1:N)
             my_plot = gplot(g, locs_x, locs_y,
             nodefillc=nodefillc, nodesize=nodesize,
-            nodelabel=nodelabel, arrowlengthfrac = 0.01,
+            nodelabel=nodelabel, arrowlengthfrac = arrowlen,
             edgestrokec=edgestrokec)
         else
             my_plot = gplot(g, locs_x, locs_y,
             nodefillc=nodefillc, nodesize=nodesize,
-            arrowlengthfrac = 0.01, edgestrokec=edgestrokec)
+            arrowlengthfrac = arrowlen, edgestrokec=edgestrokec)
         end
     end
     return my_plot
@@ -137,7 +137,7 @@ function DAG_Plot_3D(Box_pos, g)
             z1 = [Box_pos[i,:][1],Box_pos[j,:][1]]
             x1 = [Box_pos[i,:][2],Box_pos[j,:][2]]
             y1 = [Box_pos[i,:][3],Box_pos[j,:][3]]
-            display(plot!(x1, y1, z1, line=:arrow, arrow=:closed))
+            display(plot!(x1, y1, z1, line=:arrow, arrow=:closed, size = (800, 800)))
         end
     end
 end
@@ -149,21 +149,30 @@ end
 
     x_shif and y_shift need to be vectors.
 """
-function plot_G_with_unit_ball(x_shift,y_shift,r,p, alpha = 0.25)
+function plot_G_with_unit_ball(x_shift, y_shift, r, p, alpha = 0.25, colors = false, markersizes = 5)
     iters = length(x_shift)
     my_plot = plot()
-    colors = [];
-    for i in 1:length(x_shift)
-        push!(colors, RGB(rand(1)[1],rand(1)[1],rand(1)[1]))
+    if colors == false
+        colors = [];
+        for i in 1:length(x_shift)
+            push!(colors, RGB(rand(1)[1],rand(1)[1],rand(1)[1]))
+        end
+    else
+        colors = colors
     end
-    # colors = [:red, :green, :blue, :lightseagreen, :purple, :orange]
     c = 1;              # just variable for new color
     # iterate through each ball to plot
     for t in 1:iters
         # colors = [:red, :green, :blue, :purple, :brown, :orange, :lightseagreen]
-        x = collect(0:0.01:r);
-        to_plot = zeros(length(x)*4, 2);
-        y = (abs.((ones(length(x)).*r).^p-abs.(x).^p)).^(1/p);
+        if p < 0
+            x = collect(r:0.0001:5.00);
+            to_plot = zeros(length(x)*4, 2);
+            y = (x.*r)./((x.^(-p) - (ones(length(x)).*r).^(-p))).^(1/(-p));
+        else
+            x = collect(0:0.00001:r);
+            to_plot = zeros(length(x)*4, 2);
+            y = (abs.((ones(length(x)).*r).^p-abs.(x).^p)).^(1/p);
+        end
         k = 0;              # separate quadrants
         # new iteration since we need to plot one quadrant at a time
         for i in 1:4
@@ -183,30 +192,47 @@ function plot_G_with_unit_ball(x_shift,y_shift,r,p, alpha = 0.25)
         end
         s = 0
         # fill area, again one quadrant at a time
+        alpha_counter = 1
         for i in 1:4
             x_prime = to_plot[1+s:s+length(x),1];
             y_prime = to_plot[1+s:s+length(x),2];
             # plot lines
             my_plot = plot!(x_prime, y_prime,
-            color = colors[c], aspect_ratio=:equal, label = "")
+            color = colors[c], aspect_ratio=:equal, label = "", linestyle = :dash)
             # fill areas
             if i%2 == 1
-                my_plot = plot!(x_prime, ones(length(x)).*y_shift[t],
-                fillrange = y_prime, fillalpha = alpha,
-                label = "", linewidth=0, c = colors[t])
+                if alpha_counter == 1 # jus to get the darker fill in the right quadrant
+                    my_plot = plot!(x_prime, ones(length(x)).*y_shift[t],
+                    fillrange = y_prime, fillalpha = alpha+0.2,
+                    label = "", linewidth=0, c = colors[t])
+                else
+                    my_plot = plot!(x_prime, ones(length(x)).*y_shift[t],
+                    fillrange = y_prime, fillalpha = alpha,
+                    label = "", linewidth=0, c = colors[t])
+                end
             else
                 my_plot = plot!(x_prime, y_prime,
                 fillrange = ones(length(x)).*y_shift[t],
                 fillalpha = alpha, label = "",
                 linewidth=0, c = colors[t])
             end
-            if i == 4
-                my_plot = plot!(label = "p = $p")
-            end
+            alpha_counter += 1
             s+=length(x)
+        end
+        if p < 0 
+            plot!(
+                Shape([x_shift[t]-r, x_shift[t]+r, x_shift[t]+r, x_shift[t]-r], [-0.4, -0.4, 1.4, 1.4]),
+                opacity=alpha, linewidth = 0, c = colors[t], label = ""
+                )
+            plot!(
+                Shape([x_shift[t], x_shift[t]+r, x_shift[t]+r, x_shift[t]], [y_shift[t], y_shift[t], 1.4, 1.4]),
+                opacity=0.2, linewidth = 0, c = colors[t], label = ""
+                )
         end
         c+=1;
     end
-    scatter!(x_shift,y_shift, c = colors[1:length(x_shift)]) # , label = "ball $t"
+    scatter!(x_shift,y_shift, c = colors[1:length(x_shift)], label = "", markersize = markersizes)
+    plot!(xlims = (-0.4, 1.4), ylims = (-0.4, 1.4),
+    xticks = [0.0,1.0], yticks = [0.0,1.0], grid= false)
     return my_plot
 end
