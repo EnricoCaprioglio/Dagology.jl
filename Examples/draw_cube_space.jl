ec20b58193ce28498e88e0c074fa48177b778e48
Burromarmellata <enricocaprioglio@yahoo.com>
2021-09-27 00:41:25 +0100

    Update draw_cube_space.jl

--- Examples/draw_cube_space.jl
+++ Examples/draw_cube_space.jl
@@ -7,8 +7,8 @@ using LaTeXStrings
 using Distributions
 using LinearAlgebra
 using Compose
-set_default_graphic_size(20cm,20cm)
-
+set_default_graphic_size(15cm,15cm)
+gr()
 ##########################################################################
 ##############################
 # Plot a 2D cube space graph #
@@ -16,7 +16,7 @@ set_default_graphic_size(20cm,20cm)
 # this representation works only for d=2
 d = 2;
 ## Set up data (any p ∈ Z, 2 < N < 50000, 0 < perc <= 100)
-p = 1.5; N = 20; perc = 40;
+p = 2.0; N = 500; perc = 10;
 max_R = d_minkowski(ones(d), zeros(d), d, p);
 ## create the graph
 (pos, g) = cube_space_digraph(N, d, max_R*perc/100, p);
@@ -33,9 +33,9 @@ dst = longest_path_vertices[1];
 ds = dijkstra_shortest_paths(g,start,weights(g));
 shortest_path_vertices = get_shortest_path_vertices(adjlist, ds.dists, dst, pos, p);
 ## Plot final graph
-arrowlen = 0.02;
+arrowlen = 0.025;
 my_plot = DAG_plot_2D(g, pos, longest_path_vertices, 
-shortest_path_vertices, false, false, false, false, true, arrowlen)
+shortest_path_vertices, false, false, true, false, true, arrowlen)
 
 # using p = -1.2; N = 200; perc = 7;
 # we get qulitatively similar plots to cone space
@@ -45,5 +45,7 @@ shortest_path_vertices, false, false, false, false, true, arrowlen)
 ## To save the plot
 using Compose, Cairo
 # draw(PDF("motivation.pdf", 16cm, 16cm), DAG_plot_2D(g, pos, longest_path_vertices, shortest_path_vertices, false, false, true, false, true, arrowlen))
-draw(PNG("motivation.png", 16cm, 16cm), DAG_plot_2D(g, pos, longest_path_vertices, shortest_path_vertices, false, false, true, false, true, arrowlen))
+draw(PNG("C:/Users/enric/Documents/TexMaker/MSc_Dissertation/figures/cube_example.png", 16cm, 16cm),
+    DAG_plot_2D(g, pos, longest_path_vertices, shortest_path_vertices, false, false, true, false, true, arrowlen)
+)
 # draw(SVG("test_graph.svg", 16cm, 16cm), DAG_plot_2D(g, pos, longest_path_vertices, shortest_path_vertices, false, false, true, false))
