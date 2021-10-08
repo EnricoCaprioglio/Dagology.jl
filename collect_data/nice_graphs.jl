f33102761dd4e0308286759465632a645ab94dcc
Burromarmellata <enricocaprioglio@yahoo.com>
2021-10-08 16:34:35 +0100

    Update nice_graphs.jl

--- collect_data/nice_graphs.jl
+++ collect_data/nice_graphs.jl
@@ -7,6 +7,8 @@ using LaTeXStrings
 using Distributions
 using LinearAlgebra
 plotlyjs()
+set_default_graphic_size(15cm,15cm)
+
 ##########################################################################
 # plot a RGG
 N = 100 ; d = 2; R = 0.1;
@@ -27,7 +29,7 @@ g = loading["g"]
 gplot(g, x, y)
 # save figure
 using Compose, Cairo
-draw(PNG("C:/Users/enric/Documents/TexMaker/MSc_Dissertation/figures/RGG_example.pdf", 16cm, 16cm), gplot(g, x, y))
+draw(PDF("C:/Users/enric/Documents/TexMaker/MSc_Dissertation/figures/RGG_example.pdf", 20cm, 20cm), gplot(g, x, y))
 draw(PNG("C:/Users/enric/Documents/TexMaker/MSc_Dissertation/figures/RGG_example.png", 16cm, 16cm), gplot(g, x, y))
 
 # Erdos-Renyi plot
@@ -37,31 +39,36 @@ gplot(g_rand, x, y)
 gplot(g_rand, layout = spring_layout)
 # save figure
 using Compose, Cairo
-draw(PNG("C:/Users/enric/Documents/TexMaker/MSc_Dissertation/figures/rand_G_example.pdf", 20cm, 20cm), gplot(g_rand, layout = spring_layout))
-draw(PNG("C:/Users/enric/Documents/TexMaker/MSc_Dissertation/figures/rand_G_comparison.png", 20cm, 20cm), gplot(g_rand, x, y))
+draw(PDF("C:/Users/enric/Documents/TexMaker/MSc_Dissertation/figures/rand_G_example.pdf", 20cm, 20cm), gplot(g_rand, layout = spring_layout))
+draw(PDF("C:/Users/enric/Documents/TexMaker/MSc_Dissertation/figures/rand_G_comparison.png", 20cm, 20cm), gplot(g_rand, x, y))
+##########################################################################
 
 
 ##########################################################################
 ########################
 ## Figures for poster ##
 ########################
-
+using JLD
 loading = load("C:/Users/enric/Documents/Imperial/MSc_Thesis/Data//For_plots/x_and_y_theo_background.jld")
 x = loading["x"]
 y = loading["y"]
 
 
-N=20; d=2; p = 1.5; perc = 40;
+N=20; d=2; p = 1.5; perc = 40; percc = 12;
 R = Inf64;
 R_max = d_minkowski(ones(d), zeros(d), d, p)
 positions = rand(N-2,d);
 pos = vcat(zeros(d)', positions, ones(d)')
-# pos[:,1] = x
-# pos[:,2] = y
-x = pos[:,1]
-y = pos[:,2]
+pos[:,1] = x
+pos[:,2] = y
+# x = pos[:,1]
+# y = pos[:,2]
 g_complete = SimpleDiGraph(N);
 g_with_R = SimpleDiGraph(N);
+g_with_R_p_neg = SimpleDiGraph(N);
+g_with_R_p_medium = SimpleDiGraph(N);
+pp = -1.5;
+ppp = 0.75;
 for i in 1:N
     for j in 1:N
         if all(pos[i,:]-pos[j,:].<0)
@@ -71,10 +78,18 @@ for i in 1:N
             if d_minkowski(pos[j,:], pos[i,:], d, p) < R_max*perc/100;
                 add_edge!(g_with_R, i, j);
             end
+            if d_minkowski(pos[j,:], pos[i,:], d, pp) < R_max*percc/100;
+                add_edge!(g_with_R_p_neg, i, j);
+            end
+            if d_minkowski(pos[j,:], pos[i,:], d, ppp) < R_max*perc/100;
+                add_edge!(g_with_R_p_medium, i, j);
+            end
         end
     end
 end
 
+##########################################################################
+# cone
 g_cone = SimpleDiGraph(N);
 R_max = 1
 θ = +π/4;
@@ -92,8 +107,11 @@ end
 arrowlen = 0.055
 plot_g_complete = gplot(g_complete, pos[:,1], ones(N)-pos[:,2],arrowlengthfrac = arrowlen)
 plot_g_with_R = gplot(g_with_R, pos[:,1], ones(N)-pos[:,2],arrowlengthfrac = arrowlen)
+plot_g_with_R_neg_p = gplot(g_with_R_p_neg, pos[:,1], ones(N)-pos[:,2],arrowlengthfrac = arrowlen)
+plot_g_with_R_medium = gplot(g_with_R_p_medium, pos[:,1], ones(N)-pos[:,2],arrowlengthfrac = arrowlen)
 plot_g_cone = gplot(g_cone, pos[:,1], ones(N)-pos[:,2],arrowlengthfrac = arrowlen)
 
+##########################################################################
 # g_with_R
 start = 1;  # this is the source vertex of the system
 dists = my_sslp(g_with_R, topological_sort_by_dfs(g_with_R), start);
@@ -108,11 +126,14 @@ shortest_path_vertices = get_shortest_path_vertices(adjlist, ds.dists, dst, pos,
 ## Plot final graph
 my_plot = DAG_plot_2D(g_with_R, pos, longest_path_vertices, 
 shortest_path_vertices, false, false, false, false, true, arrowlen)
+using Cairo, Compose
 draw(
-    PNG("C:/Users/enric/Documents/Imperial/MSc_Thesis/Poster_figures/example_g_with_R_paths.png", 16cm, 16cm), 
+    PNG("C:/Users/enric/Documents/TexMaker/MSc_Dissertation/figures/example_g_with_R_paths.png", 16cm, 16cm), 
     DAG_plot_2D(g_with_R, pos, longest_path_vertices, 
     shortest_path_vertices, false, false, false, false, true, arrowlen)
 )
+
+##########################################################################
 # g_complete
 start = 1;  # this is the source vertex of the system
 dists = my_sslp(g_complete, topological_sort_by_dfs(g_complete), start);
@@ -128,10 +149,12 @@ shortest_path_vertices = get_shortest_path_vertices(adjlist, ds.dists, dst, pos,
 my_plot = DAG_plot_2D(g_complete, pos, longest_path_vertices, 
 shortest_path_vertices, false, false, false, false, true, arrowlen)
 draw(
-    PNG("C:/Users/enric/Documents/Imperial/MSc_Thesis/Poster_figures/example_g_complete_paths.png", 16cm, 16cm), 
+    PNG("C:/Users/enric/Documents/TexMaker/MSc_Dissertation/figures/example_g_complete_paths.png", 16cm, 16cm), 
     DAG_plot_2D(g_complete, pos, longest_path_vertices, 
     shortest_path_vertices, false, false, false, false, true, arrowlen)
 )
+
+##########################################################################
 # g_cone
 start = 1;  # this is the source vertex of the system
 dists = my_sslp(g_cone, topological_sort_by_dfs(g_cone), start);
@@ -147,11 +170,55 @@ shortest_path_vertices = get_shortest_path_vertices(adjlist, ds.dists, dst, pos,
 my_plot = DAG_plot_2D(g_cone, pos, longest_path_vertices, 
 shortest_path_vertices, false, false, false, false, true, arrowlen)
 draw(
-    PNG("C:/Users/enric/Documents/Imperial/MSc_Thesis/Poster_figures/example_g_cone_paths.png", 16cm, 16cm), 
+    PNG("C:/Users/enric/Documents/TexMaker/MSc_Dissertation/figures/example_g_cone_paths.png", 16cm, 16cm), 
     DAG_plot_2D(g_cone, pos, longest_path_vertices, 
     shortest_path_vertices, false, false, false, false, true, arrowlen)
 )
+##########################################################################
+# g_with_R_neg_p
+start = 1;  # this is the source vertex of the system
+dists = my_sslp(g_with_R_p_neg, topological_sort_by_dfs(g_with_R_p_neg), start);
+length_longest_path = maximum(dists);
+vertex_longest_path = findall(x -> x == length_longest_path, dists)[1];
+adjlist = g_with_R_p_neg.badjlist;
+longest_path_vertices = get_longest_path_vertices(adjlist, dists, pos, p);
+dst = longest_path_vertices[1];
+## Find shortest path distances
+ds = dijkstra_shortest_paths(g_with_R_p_neg,start,weights(g_with_R_p_neg));
+shortest_path_vertices = get_shortest_path_vertices(adjlist, ds.dists, dst, pos, p);
+## Plot final graph
+my_plot = DAG_plot_2D(g_with_R_p_neg, pos, longest_path_vertices, 
+shortest_path_vertices, false, false, false, false, true, arrowlen)
+using Cairo, Compose
+draw(
+    PNG("C:/Users/enric/Documents/TexMaker/MSc_Dissertation/figures/example_g_with_R_p_neg_paths.png", 16cm, 16cm), 
+    DAG_plot_2D(g_with_R_p_neg, pos, longest_path_vertices, 
+    shortest_path_vertices, false, false, false, false, true, arrowlen)
+)
 
+##########################################################################
+# g_with_R_p_medium
+start = 1;  # this is the source vertex of the system
+dists = my_sslp(g_with_R_p_medium, topological_sort_by_dfs(g_with_R_p_medium), start);
+length_longest_path = maximum(dists);
+vertex_longest_path = findall(x -> x == length_longest_path, dists)[1];
+adjlist = g_with_R_p_medium.badjlist;
+longest_path_vertices = get_longest_path_vertices(adjlist, dists, pos, p);
+dst = longest_path_vertices[1];
+## Find shortest path distances
+ds = dijkstra_shortest_paths(g_with_R_p_medium,start,weights(g_with_R_p_medium));
+shortest_path_vertices = get_shortest_path_vertices(adjlist, ds.dists, dst, pos, p);
+## Plot final graph
+my_plot = DAG_plot_2D(g_with_R_p_medium, pos, longest_path_vertices, 
+shortest_path_vertices, false, false, false, false, true, arrowlen)
+using Cairo, Compose
+draw(
+    PNG("C:/Users/enric/Documents/TexMaker/MSc_Dissertation/figures/example_g_with_R_p_medium_paths.png", 16cm, 16cm), 
+    DAG_plot_2D(g_with_R_p_medium, pos, longest_path_vertices, 
+    shortest_path_vertices, false, false, false, false, true, arrowlen)
+)
+
+##########################################################################
 # https://juliapackages.com/p/jld
 using JLD
 x = pos[:,1]
